7400aa63d1043a561d590246e0fee6c13aeb58a3
Julio Bravo <13137972+jram33@users.noreply.github.com>
2019-12-10 16:18:19 -0600

    Read log_data

--- etl.py
+++ etl.py
@@ -21,7 +21,7 @@ def process_song_file(cur, filepath):
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines = True) 
 
     # filter by NextSong action
     df = 
