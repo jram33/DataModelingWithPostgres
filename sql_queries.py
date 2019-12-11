1b58da5a768429a6fd185b9a934fc192299cae20
Julio Bravo <13137972+jram33@users.noreply.github.com>
2019-12-10 23:23:00 -0600

    Update primary keys and actions on conflict

--- sql_queries.py
+++ sql_queries.py
@@ -17,9 +17,8 @@ songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id se
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id varchar PRIMARY KEY, first_name varchar, 
                                                           last_name varchar, gender char, level varchar);""")
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, 
-                                                          artist_id varchar, year int, duration numeric,
-                                                          PRIMARY KEY (song_id, artist_id));""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, title varchar, 
+                                                          artist_id varchar, year int, duration numeric);""")
 
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, name varchar, 
                                                               location varchar, latitude numeric, 
@@ -35,10 +34,11 @@ songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, s
                                                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);""")
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
-                        VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING;""")
+                        VALUES (%s, %s, %s, %s, %s) 
+                        ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level;""")
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
-                        VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id, artist_id) DO NOTHING;""")
+                        VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING;""")
 
 artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude)
                           VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING;""")
